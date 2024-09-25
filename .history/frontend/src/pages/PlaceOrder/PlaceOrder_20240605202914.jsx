cfbed1ec01f7694b589816f18ae4b6f493d8bea1
Andrei Nagy <56216769+andrei-nagy@users.noreply.github.com>
2024-09-25 22:10:37 +0300

    first-commit Food Del 25.09.2024

---
+++ .history/frontend/src/pages/PlaceOrder/PlaceOrder_20240605202914.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+import './PlaceOrder.css'
+
+const PlaceOrder = () => {
+  return (
+    <form className="place-order">
+      <div className="place-order-left">
+        <p className="title">Delivery Information</p>
+        <div className="multi-fields">
+          <input type="text" placeholder='First Name' />
+          <input type="text" placeholder='Last Name' />
+        </div>
+        <input type="email" placeholder='Email address' />
+        <input type="text" placeholder='Street' />
+        <div className="multi-fields">
+          <input type="text" placeholder='City' />
+          <input type="text" placeholder='State' />
+        </div>
+        <div className="multi-fields">
+          <input type="text" placeholder='Zip code' />
+          <input type="text" placeholder='Country' />
+        </div>
+        <input type="text" placeholder='Phone number'/>
+      </div>
+      <div className="place-order-right">
+
+      </div>
+    </form>
+  )
+}
+
+export default PlaceOrder
